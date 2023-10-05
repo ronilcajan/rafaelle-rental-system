fcdfa738894c3271db8e1268766128e197dd1c95
Ronil Cajan <bugsgamer00@gmail.com>
2023-10-05 21:09:28 +0800

    added bootstrap templates

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Permission::create(['name' => 'edit']);
+        Permission::create(['name' => 'delete']);
+        Permission::create(['name' => 'create']);
+        Permission::create(['name' => 'view']);
+        
+        Role::create(['name' => 'super-admin']);
+        $role = Role::create(['name' => 'rental-admin']);
+        $role1 = Role::create(['name' => 'rental-manager']);
+        $role2 = Role::create(['name' => 'rental-staff']);
+
+        $role->givePermissionTo(['edit','delete','create','view']);
+        $role1->givePermissionTo(['create','view']);
+        $role2->givePermissionTo(['create','view']);
+    }
+}
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,84 @@
+@extends('layouts.auth')
+
+@push('header')
+    <style>
+        .field-icon {
+            float: right;
+            margin-top: -33px;
+            margin-right: 10px;
+            position: relative;
+            z-index: 2;
+            cursor: pointer;
+        }
+    </style>
+    {!! htmlScriptTagJsApi() !!}
+@endpush
+@section('content')
+    <div class="row w-100 mx-0">
+        <div class="col-lg-4 mx-auto">
+            <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+                <div class="brand-logo text-center">
+                    <img src="../../images/logo.svg" alt="logo">
+                </div>
+                <h4 class="text-center">Hello! let's get started</h4>
+                <h6 class="font-weight-light text-center">Sign in to continue.</h6>
+                @error('email')
+                    <div class="alert alert-fill-danger" role="alert">
+                        <i class="ti-info-alt"></i>
+                        {{ $message }}
+                    </div>
+                @enderror
+                @if ($errors->has('g-recaptcha-response'))
+                    <div class="alert alert-fill-danger" role="alert">
+                        <i class="ti-info-alt"></i>
+                        {{ $errors->first('g-recaptcha-response') }}
+                    </div>
+                @endif
+                <form class="pt-3" method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="form-group">
+                        <input type="email" name="email" class="form-control form-control-lg" id="exampleInputEmail1"
+                            placeholder="{{ __('Email Address') }}" value="{{ old('email') }}" required
+                            autocomplete="email" autofocus>
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control form-control-lg" id="password"
+                            placeholder="{{ __('Password') }}" name="password" required autocomplete="current-password">
+                        <span toggle="#password" class="field-icon toggle-password ti-lock"></span>
+                    </div>
+                    <!-- Google reCaptcha v2 -->
+                    {!! htmlFormSnippet() !!}
+                    <div class="mt-3">
+                        <button type="submit"
+                            class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn">SIGN IN</button>
+                    </div>
+                    <div class="my-2 d-flex justify-content-between align-items-center">
+                        <div class="form-check">
+                            <label class="form-check-label text-muted">
+                                <input type="checkbox" class="form-check-input" {{ old('remember') ? 'checked' : '' }}
+                                    name="remember">
+                                Keep me signed in
+                            </label>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('script')
+    <script>
+        // show password when toggle
+        $("span.toggle-password").click(function() {
+            $(this).toggleClass("ti-lock ti-unlock");
+            var input = $($(this).attr("toggle"));
+
+            if (input.attr("type") == "password") {
+                input.attr("type", "text");
+            } else {
+                input.attr("type", "password");
+            }
+        });
+    </script>
+@endpush
